419a89754347a31651b2fa1d6bfa943792f74642
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-04 21:33:34 +0200

    add ProjectTechnologySeeder

---
+++ database/seeders/ProjectTechnologySeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\Technology;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectTechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {  
+        $technologies = Technology::all()->pluck('id');
+
+        for($i = 1; $i < 40; $i++) {
+            $project = Project::find($i);
+            $project->technologies()->attach($faker->randomElements($technologies, 3));
+        }
+
+
+
+    }
+}
