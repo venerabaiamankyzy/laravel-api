de3967dcde6ecdcce7de2c391185a7aa262c7eeb
Venera Baiaman kyzy <venerabayaman@gmail.com>
2023-05-06 21:19:47 +0200

    add column Technology to show blade

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -15,27 +15,57 @@
     <div class="card-body">
       {{-- @dump($project->type?->label) --}}
       <figure class="float-end ms-5 mb-3">
-        <img src="{{ $project->getImageUri() }}" alt="{{ $project->slug}}" width="200">
+        <img src="{{ $project->getImageUri() }}" alt="{{ $project->slug}}" width="300">
           <figcaption>
             <p class="text-muted text-secondary m-0">{{ $project->slug}}</p>
           </figcaption>
       </figure>
-      <p>
-        <strong>Tipo:</strong>
-        <span class="badge rounded-pill" style="background-color: {{ $project->type?->color}} ">{{ $project->type?->label }}</span>
-      </p>
+
+      <div class="row">
+        <div class="col-3">
+          <p>
+            <strong>Tipo:</strong><br>
+            @if ($project->type)
+              {!! $project->type->getBadgeHTML() !!}
+            @else 
+              Untyped           
+            @endif        
+          </p>
+        </div>
+
+        <div class="col-3">
+          <p>
+            <strong>Technologies:</strong><br>
+            @forelse ($project->technologies as $technology)
+                {!! $technology->getBadgeHTML() !!}
+            @empty
+              No technology associated 
+            @endforelse  
+        </p>
+        </div>
+
+        <div class="col-3">
+          <p>
+            <strong>Created: </strong> <br>{{ $project->created_at }}
+        </p>
+        </div>
+
+        <div class="col-3">
+          <p>
+            <strong>Last edit: </strong> <br>{{ $project->updated_at }}
+        </p>
+        </div>
+      </div>
+      
+
+      
 
       <p>
         <strong>Contenuto:</strong>
 
         <p>{{ $project->link}}</p>
         <p>{{ $project->text}}</p>
-      </p>
-
-
-
-       
-          
+      </p>  
     </div>
   </section>  
 @endsection
